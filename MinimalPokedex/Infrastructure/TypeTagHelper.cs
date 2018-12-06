2ec9fcb8802402cccbb9951ded1b7abf204c33e5
uiucanh <bdanh96@gmail.com>
2018-12-06 20:53:56 +0000

    Add colors for types

---
+++ MinimalPokedex/Infrastructure/TypeTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinimalPokedex.Infrastructure
+{
+    [HtmlTargetElement("type")]
+    public class TypeTagHelper : TagHelper
+    {
+        public string TypeName { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            var typeName = "type" + TypeName;
+            output.Attributes.SetAttribute("class", typeName);
+            output.Content.SetContent(TypeName);
+        }
+    }
+}
